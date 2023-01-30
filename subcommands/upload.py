7ff0c3112a5fb533ac14c19718b76a36b1bba095
Eric Yablonowitz <eric.yablonowitz@gmail.com>
2023-01-30 10:01:19 -0500

    refactor upload to multiple functions

--- subcommands/upload.py
+++ subcommands/upload.py
@@ -9,13 +9,19 @@ from time import time
 BUCKET = "hardcoded-bucket"
 
 
+def upload(filepath, bucket=BUCKET):
+    body = bytes_from_file_path(filepath)
+    key = f"{int(time())}{file_extension(filepath)}"
+    put_to_s3(key, body, bucket)
+
+
 def file_extension(filepath):
     base, ext = os.path.splitext(
         os.path.basename(filepath))
     return ext
 
 
-def upload(filepath, s3_class=S3, bucket=BUCKET):
+def bytes_from_file_path(filepath):
     try:
         with open(filepath, "rb") as fh:
             body = bytes(fh.read())
@@ -23,10 +29,13 @@ def upload(filepath, s3_class=S3, bucket=BUCKET):
         sys.exit(f"Error: file {filepath} not found.")
     except PermissionError:
         sys.exit(f"Error: Permissions error reading {filepath}.")
-    key = f"{int(time())}{file_extension(filepath)}"
+    return body
+
+
+def put_to_s3(key, body, bucket):
     try:
         s3_client = boto3.client('s3')
-        s3 = s3_class(s3_client)
+        s3 = S3(s3_client)
         s3.put_object(bucket=bucket, key=key, body=body)
     except botocore.exceptions.BotoCoreError as e:
         sys.exit("Error: unable to upload file.\n" + e.fmt)
